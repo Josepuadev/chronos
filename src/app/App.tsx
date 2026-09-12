a3142c62f481fbc346ec7a73116fa1ee726f83c8
Josepuadev <josepuadev@gmail.com>
2026-09-12 10:12:03 +0200

    Empezando app chronos

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+
+  return (
+    <main>
+      <h1>Chronos</h1>
+      <p>Explora la historia a través del tiempo, mapas e historias</p>
+    </main>
+  );
+}
+
+export default App
